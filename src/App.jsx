f42ababad2606f0777eb3a1303f505e652752f90
olegtiotenshi <tiotenshi2901@gmail.com>
2024-07-08 12:34:32 +0300

    course_app add list and details info

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import TodoApp from './todo_app/TodoApp'
+import TodoApp from './components/todo_app/TodoApp'
 import Home from './components/Home'
-import About from './components/About'
 import NotFound from './components/NotFound'
 import MainLayout from './layouts/MainLayout'
+import Courses from './components/courses_app/components/Courses'
+import SingleCourse from './components/courses_app/components/SingleCourse'
 import './App.css'
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
             <Route index element={<Home />} />
             <Route path="*" element={<NotFound />} />
             <Route path="todo" element={<TodoApp />} />
-            <Route path="about" element={<About />} />
+            <Route path="courses" element={<Courses />} />
+            <Route path="courses/:courseSlug" element={<SingleCourse />} />
           </Route>
         </Routes>
       </div>
