44b23d144eb7e8f3668bbfd65c390fd710209e59
olegtiotenshi <tiotenshi2901@gmail.com>
2024-07-10 00:58:44 +0300

    book_library_app filter Redux slice and filter by title

--- src/components/book_library_app/frontend/components/BookList/BookList.js
+++ src/components/book_library_app/frontend/components/BookList/BookList.js
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators'
 import { BsBookmarkStar, BsBookmarkStarFill } from 'react-icons/bs'
+import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators'
+import { selectTitleFilter } from '../../redux/slices/filterSlice'
 import './BookList.css'
 
 const BookList = () => {
   const books = useSelector((state) => state.books)
+  const titleFilter = useSelector(selectTitleFilter)
   const dispatch = useDispatch()
 
   const handleDeleteBook = (id) => {
@@ -15,14 +17,21 @@ const BookList = () => {
     dispatch(toggleFavorite(id))
   }
 
+  const filteredBooks = books.filter((book) => {
+    const matchesTitle = book.title
+      .toLowerCase()
+      .includes(titleFilter.toLowerCase())
+    return matchesTitle
+  })
+
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
-      {books.length === 0 ? (
+      {filteredBooks.length === 0 ? (
         <p>No books available</p>
       ) : (
         <ul>
-          {books.map((book, i) => (
+          {filteredBooks.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
                 {++i}. {book.title} by <strong>{book.author}</strong>
