1387fa940ec93e4654538383ea4bb1cb89db4f26
olegtiotenshi <tiotenshi2901@gmail.com>
2024-07-10 16:40:21 +0300

    book_library_app_v2 dispatch error in the async thunk

--- src/components/book_library_app/frontend/components/BookForm/BookForm.js
+++ src/components/book_library_app/frontend/components/BookForm/BookForm.js
@@ -29,7 +29,7 @@ const BookForm = () => {
   }
 
   const handleAddRandomViaAPI = async () => {
-    dispatch(fetchBook())
+    dispatch(fetchBook('http://localhost:4000/random-book'))
   }
 
   return (
