7959ebf662ae39fd992aa6a9c9468e278c818453
olegtiotenshi <tiotenshi2901@gmail.com>
2024-07-10 23:01:17 +0300

    book_library_app_v2 moved isLoadingViaAPI to Redux

--- src/components/book_library_app/frontend/components/BookForm/BookForm.js
+++ src/components/book_library_app/frontend/components/BookForm/BookForm.js
@@ -1,7 +1,11 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { FaSpinner } from 'react-icons/fa'
-import { addBook, fetchBook } from '../../redux/slices/booksSlice'
+import {
+  addBook,
+  fetchBook,
+  selectIsLoadingViaAPI,
+} from '../../redux/slices/booksSlice'
 import { setError } from '../../redux/slices/errorSlice'
 import createBookWithID from '../../utils/createBookWithID'
 import booksData from '../../../data/book.json'
@@ -10,7 +14,7 @@ import './BookForm.css'
 const BookForm = () => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+  const isLoadingViaAPI = useSelector(selectIsLoadingViaAPI)
   const dispatch = useDispatch()
 
   const handleSubmit = (e) => {
@@ -30,14 +34,8 @@ const BookForm = () => {
     dispatch(addBook(createBookWithID(randomBook, 'random')))
   }
 
-  const handleAddRandomViaAPI = async () => {
-    setIsLoading(true)
-
-    try {
-      await dispatch(fetchBook('http://localhost:4000/random-book-delayed'))
-    } finally {
-      setIsLoading(false)
-    }
+  const handleAddRandomViaAPI = () => {
+    dispatch(fetchBook('http://localhost:4000/random-book-delayed'))
   }
 
   return (
@@ -70,9 +68,9 @@ const BookForm = () => {
         <button
           type="button"
           onClick={handleAddRandomViaAPI}
-          disabled={isLoading}
+          disabled={isLoadingViaAPI}
         >
-          {isLoading ? (
+          {isLoadingViaAPI ? (
             <>
               <span>Loading Book...</span>
               <FaSpinner className="spinner" />
--- src/components/book_library_app/frontend/redux/slices/booksSlice.js
+++ src/components/book_library_app/frontend/redux/slices/booksSlice.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 import createBookWithID from '../../utils/createBookWithID'
 import { setError } from './errorSlice'
 
-const initialState = []
+const initialState = {
+  books: [],
+  isLoadingViaAPI: false,
+}
 
 export const fetchBook = createAsyncThunk(
   'books/fetchBook',
@@ -13,7 +16,7 @@ export const fetchBook = createAsyncThunk(
       return res.data
     } catch (error) {
       thunkAPI.dispatch(setError(error.message))
-      throw error
+      return thunkAPI.rejectWithValue(error)
     }
   }
 )
@@ -23,13 +26,16 @@ const booksSlice = createSlice({
   initialState,
   reducers: {
     addBook: (state, action) => {
-      state.push(action.payload)
+      state.books.push(action.payload)
     },
     deleteBook: (state, action) => {
-      return state.filter((book) => book.id !== action.payload)
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== action.payload),
+      }
     },
     toggleFavorite: (state, action) => {
-      state.forEach((book) => {
+      state.books.forEach((book) => {
         if (book.id === action.payload) {
           book.isFavorite = !book.isFavorite
         }
@@ -37,16 +43,24 @@ const booksSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
+    builder.addCase(fetchBook.pending, (state) => {
+      state.isLoadingViaAPI = true
+    })
     builder.addCase(fetchBook.fulfilled, (state, action) => {
+      state.isLoadingViaAPI = false
       if (action.payload.title && action.payload.author) {
-        state.push(createBookWithID(action.payload, 'API'))
+        state.books.push(createBookWithID(action.payload, 'API'))
       }
+    })
+    builder.addCase(fetchBook.rejected, (state) => {
+      state.isLoadingViaAPI = false
     })
   },
 })
 
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions
 
-export const selectBooks = (state) => state.books
+export const selectBooks = (state) => state.books.books
+export const selectIsLoadingViaAPI = (state) => state.books.isLoadingViaAPI
 
 export default booksSlice.reducer
